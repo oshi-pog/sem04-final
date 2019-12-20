f4de88d33137416d65147781dfa361560ae65484
Joshua Kaplan <joshkap2015@gmail.com>
2019-12-20 07:49:38 +0100

    Testing

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/navbar/navbar'
 import LandingHero from './components/LandingHero/LandingHero';
 import LandingGrid from './components/LandingGrid/LandingGrid';
 import WhatWeDo from '././components/WhatWeDo/WhatWeDo';
